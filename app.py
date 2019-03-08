f50d609e3d134865015651a7eab52212d11ffdcc
Nijar <nijarixa@gmail.com>
2019-03-08 09:49:30 +0700

    Simple API Python Flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return __name__
+
+@app.route("/home")
+def home():
+	return "Hello World"
+
+if __name__ == '__main__':
+	app.run(debug=True)
